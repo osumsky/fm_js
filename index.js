10501577125407f5572d01030cfe4a4ab3c0c268
Олег <osumsky@gmail.com>
2021-09-27 11:52:58 +0300

    result of tasks

---
+++ index.js
@@ -0,0 +1,37 @@
+// 1st task
+
+let firstNum = 45;
+let secondNum = 12;
+
+let result = firstNum + secondNum;
+console.log("result =", result);
+
+result = firstNum - secondNum;
+console.log("result =", result);
+
+result = firstNum * secondNum;
+console.log("result =", result);
+
+result = firstNum / secondNum;
+console.log("result =", result);
+
+result = firstNum % secondNum;
+console.log("result =", result);
+
+// 2nd task
+
+let number = 101;
+let isAdult = true;
+let languageName = "Java";
+let userName = "Elon Musk";
+let empty = null;
+
+// 3rd task
+
+let number = 23;
+number += 11;
+number /= 2;
+number -= 1;
+number *= 11;
+number += 1;
+number %= 5;

7d9f8ebf810e3684674cf434f65a4a53fe3ba8a9
Олег <osumsky@gmail.com>
2021-09-27 11:56:51 +0300

    Fixed errors

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 // 1st task
 
-let firstNum = 45;
-let secondNum = 12;
+let firstNumber = 45;
+let secondNumber = 12;
 
-let result = firstNum + secondNum;
+let result = firstNumber + secondNumber;
 console.log("result =", result);
 
-result = firstNum - secondNum;
+result = firstNumber - secondNumber;
 console.log("result =", result);
 
-result = firstNum * secondNum;
+result = firstNumber * secondNumber;
 console.log("result =", result);
 
-result = firstNum / secondNum;
+result = firstNumber / secondNumber;
 console.log("result =", result);
 
-result = firstNum % secondNum;
+result = firstNumber % secondNumber;
 console.log("result =", result);
 
 // 2nd task
 
-let number = 101;
-let isAdult = true;
+let num = 101;
+let logicalVar = true;
 let languageName = "Java";
 let userName = "Elon Musk";
 let empty = null;
@@ -31,7 +31,7 @@ let empty = null;
 let number = 23;
 number += 11;
 number /= 2;
-number -= 1;
+number--;
 number *= 11;
-number += 1;
+number++;
 number %= 5;

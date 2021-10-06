2457a6ea879cfde04753cd7893f5b4ebd3f0b11b
Олег <osumsky@gmail.com>
2021-10-06 11:49:32 +0300

    Add Prototype

--- index.js
+++ index.js
@@ -1,37 +1,30 @@
-// 1st task
+/* logic */
+function MyArrayProto() {
+  this.push = function () {
+    for (let i=0; i<arguments.length; i++) {
+      this[this.length++] = arguments[i];
+    }
+    return this.length;
+  };
+  this.pop = function () {
+    if (this.length === 0) {
+      return;
+    }
+    const item = this[--this.length];
+    delete this[this.length];
+    return item;
+  };
+}
+/* data */
+function MyArray() {
+  this.length = 0;
+  for (let i=0; i<arguments.length; i++) {
+    this.push(arguments[i]);
+  }
+}
+/* Prototype*/
+MyArray.prototype = new MyArrayProto();
+MyArray.prototype.newMethod = function(){};
 
-let firstNumber = 45;
-let secondNumber = 12;
-
-let result = firstNumber + secondNumber;
-console.log("result =", result);
-
-result = firstNumber - secondNumber;
-console.log("result =", result);
-
-result = firstNumber * secondNumber;
-console.log("result =", result);
-
-result = firstNumber / secondNumber;
-console.log("result =", result);
-
-result = firstNumber % secondNumber;
-console.log("result =", result);
-
-// 2nd task
-
-let num = 101;
-let logicalVar = true;
-let languageName = "Java";
-let userName = "Elon Musk";
-let empty = null;
-
-// 3rd task
-
-let number = 23;
-number += 11;
-number /= 2;
-number--;
-number *= 11;
-number++;
-number %= 5;
+const myArray = new MyArray(1,1,1,15,5);
+myArray.push(2,2,2);
